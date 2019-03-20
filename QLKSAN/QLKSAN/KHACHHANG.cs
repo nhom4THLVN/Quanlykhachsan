5e0167f989e6f86d6668655f882391c9d41b36fa
nhom4THLVN <48239687+nhom4THLVN@users.noreply.github.com>
2019-03-20 20:12:45 +0700

    Create KHACHHANG.cs

---
+++ QLKSAN/QLKSAN/KHACHHANG.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QLKSAN
+{
+    public class KHACHHANG
+    {
+        string cmnd;
+
+        public string Cmnd
+        {
+            get { return cmnd; }
+            set { cmnd = value; }
+        }
+        string hoten;
+
+        public string Hoten
+        {
+            get { return hoten; }
+            set { hoten = value; }
+        }
+        string gioitinh;
+
+        public string Gioitinh
+        {
+            get { return gioitinh; }
+            set { gioitinh = value; }
+        }
+        string sdt;
+
+        public string Sdt
+        {
+            get { return sdt; }
+            set { sdt = value; }
+        }
+        string diachi;
+
+        public string Diachi
+        {
+            get { return diachi; }
+            set { diachi = value; }
+        }
+    }
+}
